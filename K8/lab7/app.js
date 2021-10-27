d1cd21415146ce59393f05f85a6e7ec631d98afb
Kaltblotsin <Kaltblotsin@gmail.com>
2021-10-28 00:35:02 +0300

    K8(edit)

--- K8/lab7/app.js
+++ K8/lab7/app.js
@@ -24,3 +24,27 @@ mouseOverContainer.onmousemove = function (e) {
         transformElement(ex1Layer, position);
     });
 }
+
+let isClicked = false;
+document.querySelector("#link").onclick = () => {
+    if (isClicked) {
+        document.querySelector("#link").href = "d:\\My documents\\Code\\Labs\\K3\\index.html";
+        document.querySelector("#link").style.color = "#8B0000";
+    }
+    isClicked = true;
+}
+
+document.querySelector("#linq").onmouseover = () => {
+    document.querySelector("#linq").style.color = "green";
+    document.querySelector("#linq").href = "d:\\My documents\\Code\\Labs\\K4\\index.html";
+}
+
+document.querySelector("#linq").onmouseout = () => {
+    document.querySelector("#linq").style.color = "black";
+    document.querySelector("#linq").href = "";
+}
+
+document.querySelector("#linc").onclick = () => {
+    document.querySelector("#linc").style.color = "rgb(184, 134, 11)";
+    document.querySelector("#linc").href = "block.html";
+}
