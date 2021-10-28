fa22b6f4bd35bdc9b5497d4404484d68d325757d
Kaltblotsin <Kaltblotsin@gmail.com>
2021-10-28 13:32:11 +0300

    K8

--- K8/scripts/script.js
+++ K8/scripts/script.js
@@ -1,1 +1,11 @@
-﻿
+﻿document.querySelector("#Form").onclick = () => {
+    document.location = "D:\\My documents\\Code\\Labs\\K4\\page0.html";
+}
+
+document.querySelector("#Chess").onclick = () => {
+    document.location = "D:\\My documents\\Code\\Labs\\K4\\index.html#Chess";
+}
+
+document.querySelector("#Photo").onclick = () => {
+    document.location = "D:\\My documents\\Code\\Labs\\K6\\pages\\page0.html";
+}
