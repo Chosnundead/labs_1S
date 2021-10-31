96715260489172c3129ba6fffd7d36477a5b3fbd
Kaltblotsin <Kaltblotsin@gmail.com>
2021-10-31 22:34:20 +0300

    T9-10(edit)

---
+++ T1-8/lab9-10/page0/scripts/script.js
@@ -0,0 +1,18 @@
+﻿$("#container").on("mousedown", () => {
+    $("#container").on("selectstart", () => {
+        return false;
+    });
+    isBreak = false;
+    $("#block").css("left", event.clientX - 100);
+    $("#block").css("top", event.clientY - 100);
+    $("#container").on("mousemove", () => {
+        $("#container").on("mouseup", () => {
+            isBreak = true;
+        });
+        if (isBreak) {
+            $("#container").off("mousemove");
+        }
+        $("#block").css("left", event.clientX - 100);
+        $("#block").css("top", event.clientY - 100);
+    });
+});
---
+++ T1-8/lab9-10/page2/scripts/script.js
@@ -0,0 +1,23 @@
+﻿let width = 1480;
+let height = 640;
+let dotBoxSize = 20;
+
+function _graphMaker(stringOfFunction) {
+    let stringOfGraph = "", coordinateX, coordinateY, x;
+    for (let stepOfFunction = (0 - (width / (dotBoxSize * 2))); stepOfFunction <= (width / (dotBoxSize * 2)); stepOfFunction++) {
+        x = stepOfFunction;
+        coordinateX = Math.abs((stepOfFunction + (width / (dotBoxSize * 2))) * dotBoxSize);
+        coordinateY = ((height / (dotBoxSize * 2)) - eval(stringOfFunction)) * dotBoxSize;
+        console.log("Y(" + stepOfFunction + "):" + coordinateY);
+        if ((coordinateY <= 640) && (coordinateY >= 0)) {
+            stringOfGraph += "<img class='image' src='images/dot.png' style='left: " + coordinateX + "px; top: " + coordinateY + "px;' />";
+        }
+    }
+    $("#container").html(stringOfGraph);
+}
+
+$("input#radioFunction").on("click", () => {
+    for (let check = 0; check < 4; check++) {
+        if
+    }
+});
