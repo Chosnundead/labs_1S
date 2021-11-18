40ef89b613f8e742f0f8d7d416ed5ee67869b3a9
Kaltblotsin <Kaltblotsin@gmail.com>
2021-11-19 00:06:22 +0300

    K10

---
+++ K10/page1/scripts/script.js
@@ -0,0 +1,17 @@
+﻿document.querySelector("#o-0").setAttribute("src", "videos/video0.mp4");
+document.querySelector("#o-1").setAttribute("src", "songs/song0.mp3");
+
+document.querySelector("#b-0").onclick = () => {
+    document.querySelector("#o-0").setAttribute("src", "videos/video0.mp4");
+    document.querySelector("#o-1").setAttribute("src", "songs/song0.mp3");
+}
+
+document.querySelector("#b-1").onclick = () => {
+    document.querySelector("#o-0").setAttribute("src", "videos/video1.mp4");
+    document.querySelector("#o-1").setAttribute("src", "songs/song1.mp3");
+}
+
+document.querySelector("#b-2").onclick = () => {
+    document.querySelector("#o-0").setAttribute("src", "videos/video2.mp4");
+    document.querySelector("#o-1").setAttribute("src", "songs/song2.mp3");
+}
