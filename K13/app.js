64cbc27b5538b9753f317a3a4fd40d1d0b012179
Kaltblotsin <Kaltblotsin@gmail.com>
2021-12-08 00:39:12 +0300

    K13

---
+++ K13/app.js
@@ -0,0 +1,11 @@
+﻿let doc = new File([""], "data.xml");
+let reader = new FileReader();
+reader.readAsText(doc);
+reader.onloadend = (event) => {
+    console.log(reader.result);
+}
+//reader.addEventListener("loadend", (event) => {
+//    console.log(reader.result);
+//});
+//let parser = new DOMParser();
+//let doc = parser.parseFromString("data.xml", "text/xml");
